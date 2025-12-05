71ffb86c12dab8c059c69a6dea1e6cff6e0fce5b
Laszlo Mile <laszlo@xcite.ltd>
2025-12-05 11:44:44 +0100

    wp

--- src/services/const.js
+++ src/services/const.js
@@ -1,5 +1,5 @@
-export const API_BASE_FOODSYNC_URL = 'https://71eeb7ef8f91.ngrok-free.app'
-// const API_BASE_FOODSYNC_URL = 'https://foodsync-api.vercel.app'
+// export const API_BASE_FOODSYNC_URL = 'https://71eeb7ef8f91.ngrok-free.app'
+export const API_BASE_FOODSYNC_URL = 'https://foodsync-api.vercel.app'
 
 export const API_BO_BASE_URL = `${API_BASE_FOODSYNC_URL}/backoffice`
 export const API_FE_BASE_URL = `${API_BASE_FOODSYNC_URL}/foodsync`
