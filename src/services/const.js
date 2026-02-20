85c26296e8d77c1785eb9309f0a82e9e125d67d9
lucsoft.sl <laszlo@xcite.ltd>
2026-02-20 14:09:21 +0100

    wp

--- src/services/const.js
+++ src/services/const.js
@@ -1,5 +1,5 @@
-// export const API_BASE_FOODSYNC_URL = 'https://foodsync-api-zeta.vercel.app'
-export const API_BASE_FOODSYNC_URL = 'http://localhost:3001'
+export const API_BASE_FOODSYNC_URL = 'https://foodsync-api-zeta.vercel.app'
+// export const API_BASE_FOODSYNC_URL = 'http://localhost:3001'
 
 export const API_BO_BASE_URL = `${API_BASE_FOODSYNC_URL}/backoffice`
 export const API_FE_BASE_URL = `${API_BASE_FOODSYNC_URL}/foodsync`
